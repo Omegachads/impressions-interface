9da360903797bd5e1a2cf00c1d69f58b5c00b3a3
Kenk <kenghin_lim@hotmail.com>
2022-11-06 06:35:22 +0000

    interface for chat service

---
+++ src/services/chat.service.ts
@@ -0,0 +1,8 @@
+// Send Message
+function sendMessage(receiver) {}
+
+// Receive XMTP message stream (conversation)/
+function receiveMessageStream(sender, receiver) {}
+
+// Authenticate user
+function authenticate(): void {}
